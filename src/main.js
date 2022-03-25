030d89bf7af14f2db9ba0f98ea3e2bcd07f2793f
Jon Rojí <joroji@MacBook-Pro-de-Jon.local>
2022-03-25 19:24:14 +0100

    remove dependencies

--- src/main.js
+++ src/main.js
@@ -1,17 +1,6 @@
-console.log('hola parcel')
-
-import './sass/site.scss';
-import './sass/style.scss';
-import './sass/header.scss';
-import './sass/utilities.scss';
-import './sass/footer.scss';
-import './sass/nav.scss';
-
 import { saludar } from "./module.js";
 
 console.log(saludar())
-
-
 
     document.getElementById("burger").addEventListener("click", (event) => {
         document.getElementById("menu").classList.toggle("active");
