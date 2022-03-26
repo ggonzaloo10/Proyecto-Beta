1ca995358d56353f33401ef8550dce6c0b3919cc
ggonzaloo10 <92878148+ggonzaloo10@users.noreply.github.com>
2022-03-26 17:17:23 +0100

    Update info.html

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import { saludar } from "./module.js";
+
+console.log(saludar())
+
+    document.getElementById("burger").addEventListener("click", (event) => {
+        document.getElementById("menu").classList.toggle("active");
+      });
+    
+    
+
---
+++ src/js/styles.js
@@ -0,0 +1,10 @@
+
+
+import '../sass/site.scss';
+import '../sass/style.scss';
+import '../sass/header.scss';
+import '../sass/utilities.scss';
+import '../sass/footer.scss';
+import '../sass/nav.scss';
+import '../sass/mediaqueries.scss';
+import '../sass/components.scss';
