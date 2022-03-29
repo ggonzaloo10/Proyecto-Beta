a0c40065bf3e1b7ba71515afbf7771b3dfbf98c2
ggonzaloo10 <92878148+ggonzaloo10@users.noreply.github.com>
2022-03-29 23:39:12 +0200

    menu icon finished correct

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("burger").addEventListener("click", (event) =>{
-document.getElementById("menu").classList.toggle("active");
-      });
-    
-document.getElementById("logo").addEventListener("click", (event) =>{
-window.open("../pages/index.html",'_self');
-      });
---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+document.getElementById("menu-icon").addEventListener("click", (event) =>{   
+document.getElementById("menu").classList.toggle("active");
+document.getElementById("cross").classList.toggle("active");
+document.getElementById("cross").classList.toggle("hidden");
+document.getElementById("burger").classList.toggle("hidden");
+      });
+    
+document.getElementById("logo").addEventListener("click", (event) =>{
+window.open("../pages/index.html",'_self');
+      });
