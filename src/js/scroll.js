372382e9d87025b8909d25efe103c85d057a1538
ggonzaloo10 <92878148+ggonzaloo10@users.noreply.github.com>
2022-04-04 10:45:40 +0200

    update pages

---
+++ src/js/scroll.js
@@ -0,0 +1,24 @@
+
+window.sr = ScrollReveal();
+
+sr.reveal('.reveal',{
+
+    duration: 2000,
+    origin: 'bottom',
+    
+})
+
+sr.reveal('.card',{
+
+      duration: 2000,
+      origin: 'bottom',
+      
+})
+
+sr.reveal('.reveal-1',{
+
+      duration: 2000,
+      origin: 'top',
+      delay: '1000',
+      distance: '200px'
+})
